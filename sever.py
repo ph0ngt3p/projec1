281b0dee0aa6a4de9ecfe4b31cbda64fc80fc0f6
hai98th <haith98pro@gmail.com>
2019-01-02 00:00:01 +0700

    aa

--- sever.py
+++ sever.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, request, g
 from flask_cors import CORS
 import sqlite3 as sql
 import json
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 CORS(app)
- 
+
 #api get all personnel
 @app.route('/api/personnel', methods = ['GET'])
 def personnel():
 	if request.method == 'GET':
-	   	con = sql.connect("database.db")
-	   	con.row_factory = sql.Row   
-	   	cur = con.cursor()
-	   	cur.execute("select * from personnel")
-	   
-	   	rows = cur.fetchall();
-		psn = {
-			'personnel': []
+		con = sql.connect("database.db")
+		con.row_factory = sql.Row   
+		cur = con.cursor()
+		cur.execute("select * from personnel")
+		rows = cur.fetchall();
+		psn={
+		'personnel':[]
 		}
 
 		for row in rows:
@@ -32,7 +32,7 @@ def personnel():
 				'sounds':row['sounds']
 				})
 		con.close()
-	   	return json.dumps(psn)
+		return json.dumps(psn)
 #api post a personnel
 @app.route('/api/personnel',methods=['POST'])
 def add_person():
@@ -46,11 +46,11 @@ def add_person():
 		sounds=request.json['sounds']
 
 		with sql.connect("database.db") as con:
-		    cur = con.cursor()
-		    cur.execute("INSERT INTO personnel (ava_url,name,phone,email,position,part,sounds) VALUES (?,?,?,?,?,?,?)",(ava_url,name,phone,email,position,part,sounds))
-		    con.commit()
-		   
+			cur = con.cursor()
+			cur.execute("INSERT INTO personnel (ava_url,name,phone,email,position,part,sounds) VALUES (?,?,?,?,?,?,?)",(ava_url,name,phone,email,position,part,sounds))
+			con.commit()
 		return "Person Added"
+
 #api put a person
 @app.route('/api/personnel/id=<int:id>',methods=['PUT'])
 def put_person(id):
@@ -86,12 +86,12 @@ def del_person(id):
 @app.route('/api/room', methods = ['GET'])
 def room():
 	if request.method == 'GET':
-	   	con = sql.connect("database.db")
-	   	con.row_factory = sql.Row   
-	   	cur = con.cursor()
-	   	cur.execute("SELECT * FROM room")
-	   
-	   	rows = cur.fetchall();
+		con = sql.connect("database.db")
+		con.row_factory = sql.Row   
+		cur = con.cursor()
+		cur.execute("SELECT * FROM room")
+
+		rows = cur.fetchall();
 		r = {
 			'room': []
 		}
@@ -104,7 +104,7 @@ def room():
 				'description':row['description']
 				})
 		con.close()
-	   	return json.dumps(r)
+		return json.dumps(r)
 #api post a room
 @app.route('/api/room',methods=['POST'])
 def add_room():
@@ -150,12 +150,11 @@ def del_room(id):
 @app.route('/api/meeting', methods = ['GET'])
 def meeting():
 	if request.method == 'GET':
-	   	con = sql.connect("database.db")
-	   	con.row_factory = sql.Row   
-	   	cur = con.cursor()
-	   	cur.execute("select * from meeting")
-	   
-	   	rows = cur.fetchall();
+		con = sql.connect("database.db")
+		con.row_factory = sql.Row   
+		cur = con.cursor()
+		cur.execute("select * from meeting")
+		rows = cur.fetchall();
 		m = {
 			'meeting': []
 		}
@@ -172,7 +171,7 @@ def meeting():
 				'secretary': row['secretary']
 				})
 		con.close()
-	   	return json.dumps(m)
+		return json.dumps(m)
 #api create a meeting
 @app.route('/api/meeting', methods=['POST'])
 def add_meeting():
@@ -226,12 +225,11 @@ def del_meeting(id):
 @app.route('/api/detail_meeting/meeting_id=<int:id>', methods = ['GET'])
 def detail(id):
 	if request.method == 'GET':
-	   	con = sql.connect("database.db")
-	   	con.row_factory = sql.Row   
-	   	cur = con.cursor()	
-	   	cur.execute("select * from detail where meeting_id = %d" %id)
-	   
-	   	rows = cur.fetchall();
+		con = sql.connect("database.db")
+		con.row_factory = sql.Row   
+		cur = con.cursor()	
+		cur.execute("select * from detail where meeting_id = %d" %id)
+		rows = cur.fetchall();
 		d = {
 			'detail': []
 		}
@@ -245,7 +243,7 @@ def detail(id):
 				'content': row['content']
 				})
 		con.close()
-	   	return json.dumps(d)
+		return json.dumps(d)
 #api them 1 loi thoai
 @app.route('/api/detail_meeting/meeting_id=<int:id>', methods = ['POST'])
 def add_detail(id):
@@ -285,7 +283,14 @@ def delete_detail(id,id2):
 
 		    return "Detail Deleted"
 
+#api upload file
+@app.route('/api/uploader', methods = ['POST'])
+def uploader_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		return "file uploaded successfully"
 
 
 if __name__ == '__main__':
-   app.run( host='0.0.0.0', port=5000, debug=True) 
+	app.run( host='0.0.0.0', port=5000, debug=True)
